7352f0b2d1da787df91074ee3e2c4e0e1191ea0e
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2022-09-19 15:41:31 +0200

    Centered  Logo Implementation

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import coffeelogo from '../assets/coffee-to-go.svg'
+
+export default function Header() {
+  const headerStyles = {
+    display: 'flex',
+    justifyContent: 'center',
+  }
+
+  const textStyle = {
+    color: '#611707',
+    fontSize: '30px',
+    fontWeight: '1000',
+  }
+
+  const logoStyle = {
+    width: '40px',
+    margin: '0 10px',
+  }
+
+  return (
+    <div style={headerStyles}>
+        <img src={coffeelogo} style={logoStyle} alt="logo" />
+        <span style={textStyle}>Coffee Tasks</span>
+    </div>
+  )
+}
