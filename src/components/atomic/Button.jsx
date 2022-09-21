24ce2031576a49cfaf01698e98a513401ea8649b
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2022-09-21 21:58:55 +0200

    props to Button component

--- src/components/atomic/Button.jsx
+++ src/components/atomic/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function Button() {
+export default function Button({margin, title, isColorFlipped}) {
 
   const button = {
-    backgroundColor: '#611707',
-    color: '#fff',
+    backgroundColor: isColorFlipped ? '#fff' : '#611707',
+    color: isColorFlipped? '#611707' : '#fff',
     padding: '6px 12px',
     border: ' 1px solid #611707',
     borderRadius: '5px',
@@ -12,11 +12,12 @@ export default function Button() {
     fontWeight: '600',
     fontFamilly: 'Poppins',
     letterSpacing: '1px',
+    margin: `${margin}`,
   } 
 
   return (
     <div>
-        <button style={button}>Add Task</button>
+        <button style={button}>{title}</button>
     </div>
   )
 }
