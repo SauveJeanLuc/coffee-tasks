5a36b91e524f5e44c7e9466641306e81e065d47c
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2022-09-21 21:59:09 +0200

    Implement input component

---
+++ src/components/atomic/Input.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Input() {
+  const inputStyles = {
+    padding: '10px 0px',
+    border: '1px solid #611707',
+    borderRadius: '5px',
+    marginBottom: '10px',
+    width: '100%',
+  }
+
+  const containerStyles = {
+    width: '100%',
+  }
+
+  return (
+    <div style={containerStyles}>
+        <input type="text" style={inputStyles} placeholder="Add a new task" />
+    </div>
+  )
+}
