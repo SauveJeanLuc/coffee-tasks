142d89de7b344e12498e40cc95db494717558d59
aadi58002 <aadi58002gmail.com>
2022-10-01 07:08:35 +0530

    Removing the dulpicate entry error with padding

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -44,9 +44,8 @@ export default function PopUp () {
 
   const popUpStyles = {
     backgroundColor: '#E5D8D8',
-    padding: '10px',
     borderRadius: '5px',
-    padding: '1em 2em 2em 2em',
+    padding: '1em 2em 2em',
   }
 
 
