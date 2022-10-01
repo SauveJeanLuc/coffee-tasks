2bea7234061bea6455f2b1d37bf0654debdc708b
aadi58002 <aadi58002gmail.com>
2022-10-01 07:16:59 +0530

    Adding pnpm-lock to ignore. Placeholder text padding fix

--- src/components/atomic/Input.jsx
+++ src/components/atomic/Input.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Input({onChangeHandler}) {
   return (
     <div className='w-full'>
-        <input onChange={onChangeHandler} type="text" className='border rounded-[5px] w-full mb-[10px] py-[10px] px-0 border-[#611707]' placeholder="Add a new task" />
+      <input onChange={onChangeHandler} type="text" className='border rounded-[5px] w-full mb-[10px] py-[10px] px-0 border-[#611707] placeholder:px-[5px]' placeholder="Add a new task" />
     </div>
   )
 }
