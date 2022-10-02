ffd31c36dfec57c44d2a609c59871ef51ffd79b2
Udoka <udochukwukaonyela@gmail.com>
2022-10-02 23:58:53 +0100

    added styles in tailwind

--- src/components/ToDoContainer.jsx
+++ src/components/ToDoContainer.jsx
@@ -1,21 +1,9 @@
 import React from "react";
 
 export default function ToDoContainer() {
-  const todoContainerStyles = {
-    backgroundColor: "#E5D8D8",
-    padding: "10px 20px",
-    textAlign: "center",
-    borderRadius: "5px",
-  };
-
-  const spanStyle = {
-    color: "#fff",
-    fontWeight: "600",
-  };
-
   return (
-    <div style={todoContainerStyles}>
-      <span style={spanStyle}>No ToDos</span>
+    <div className="bg-coffeePrimaryLight px-5 py-2.5 text-center rounded-md">
+      <span className="text-white font-semibold">No ToDos</span>
     </div>
   );
 }
