d6d2e677c016aa8aa4b07617e32e8353c3e3004c
Udoka <udochukwukaonyela@gmail.com>
2022-10-02 23:48:34 +0100

    added tailwind styles and removed props

--- src/components/atomic/Button.jsx
+++ src/components/atomic/Button.jsx
@@ -1,37 +1,16 @@
-import PropTypes from 'prop-types'
-import React, { useState } from "react"
+import PropTypes from "prop-types";
+import React from "react";
 
-export default function Button({
-  margin,
-  title,
-  isColorFlipped,
-  clickHandler,
-}) {
-  const [hover, setHover] = useState(false);
-
-  const button = {
-    cursor: 'pointer',
-    backgroundColor: isColorFlipped ? '#fff' : '#611707',
-    color: isColorFlipped? '#611707' : '#fff',
-    padding: '6px 12px',
-    border: ' 1px solid #611707',
-    borderRadius: '5px',
-    fontSize: '15px',
-    fontWeight: '600',
-    fontFamily: 'Poppins',
-    letterSpacing: '1px',
-    margin: `${margin}`,
-    transform: hover && "translateY(-3px)",
-    boxShadow: hover && "0px 2px 2px 2px rgb(97, 23, 7)",
-  };
-
+export default function Button({ title, isColorFlipped, clickHandler }) {
   return (
     <div>
       <button
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
         onClick={clickHandler}
-        style={button}
+        className={`cursor-pointer px-3 py-1.5 border font-poppins  border-coffeeDark rounded-md text-base font-semibold tracking-wide ${
+          isColorFlipped
+            ? `bg-white text-coffeeDark`
+            : `bg-coffeeDark text-white`
+        }   hover:-translate-y-1 hover:shadow-md transition-all duration-500 ease-in hover:shadow-coffeeDark/45`}
       >
         {title}
       </button>
@@ -43,5 +22,5 @@ Button.propTypes = {
   margin: PropTypes.string,
   title: PropTypes.string,
   isColorFlipped: PropTypes.bool,
-  clickHandler: PropTypes.func
-}
+  clickHandler: PropTypes.func,
+};
