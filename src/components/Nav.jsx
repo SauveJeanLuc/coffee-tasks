8c2f47d31d06e412b01ce12866ee22638a595824
Prakhar-Goyal-3014 <prakhargoyal.3014@gmail.com>
2022-10-07 11:16:27 +0530

    Responsiveness corrected

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
-import Button from "./atomic/Button";
-import SelectButton from "./atomic/SelectButton";
-import PopUp from "./PopUp";
-import propTypes from "prop-types";
+import React, { useState } from 'react'
+import Button from './atomic/Button'
+import SelectButton from './atomic/SelectButton'
+import PopUp from './PopUp'
+import propTypes from 'prop-types'
 export default function Nav({ setTodos }) {
-  const [popUpvisible, setPopUpvisible] = useState(false);
+  const [popUpvisible, setPopUpvisible] = useState(false)
 
   const handlePopUpOpen = () => {
-    setPopUpvisible(true);
-  };
+    setPopUpvisible(true)
+  }
 
   return (
     <div className="flex justify-between px-5 py-2.5 border-b-2 border-coffeeDark">
@@ -17,16 +17,18 @@ export default function Nav({ setTodos }) {
         trigger={setPopUpvisible}
         setTodos={setTodos}
       />
-      <Button
-        clickHandler={handlePopUpOpen}
-        className="mr-3 text-[13px] lg:text-[18px]"
-        title={"Add task"}
-      />
-      <SelectButton width={"155px"} className="sm:w-10/12" />
+      <div className='w-[100%] flex justify-between align-middle'>
+        <Button
+          clickHandler={handlePopUpOpen}
+          className="mt-[2px] mr-3 text-[10px] lg:text-[18px]"
+          title={'Add task'}
+        />
+        <SelectButton width={'155px'} className="sm:w-10/12" />
+      </div>
     </div>
-  );
+  )
 }
 
 Nav.propTypes = {
   setTodos: propTypes.func.isRequired,
-};
+}
