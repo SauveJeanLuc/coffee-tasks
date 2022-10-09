b29e5ce815831869dde46a4403909c80382da822
Richard <richard.m.hpa@gmail.com>
2022-10-10 11:35:32 +1300

    added prettier into project

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const Todo = () => {
   return <div>Todo</div>;
--- src/components/atomic/Input.jsx
+++ src/components/atomic/Input.jsx
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 
 export default function Input({ onChangeHandler, value, placeholder = 'Add a new task' }) {
   return (
-    <div className="w-full">
+    <div className='w-full'>
       <input
         onChange={onChangeHandler}
-        type="text"
-        className="p-2 border-2 border-coffeeDark rounded-md w-full my-3"
+        type='text'
+        className='p-2 border-2 border-coffeeDark rounded-md w-full my-3'
         placeholder={placeholder}
         value={value}
       />
