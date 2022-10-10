627d63b97457e8fc30e25484bae38a49a74d6d4c
suryansh-bhatnagar <sbh2068@gmail.com>
2022-10-10 13:30:43 +0530

    UX changes done

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -17,15 +17,26 @@ export default function Nav({ setTodos, setFilter }) {
   };
 
   return (
-    <div className="flex justify-between px-5 py-2.5 border-b-2 border-coffeeDark">
-      <PopUp visible={popUpvisible} trigger={setPopUpvisible} setTodos={setTodos} />
-      <div className="w-[100%] flex justify-between align-middle">
+    <div className='flex justify-between px-5 py-2.5 border-b-2 border-coffeeDark'>
+      <PopUp
+        visible={popUpvisible}
+        trigger={setPopUpvisible}
+        setTodos={setTodos}
+      />
+      <div className='w-[100%] flex justify-between align-middle'>
         <Button
           clickHandler={handlePopUpOpen}
-          className="mt-[2px] mr-3 text-[8px] sm:text-[10px] lg:text-[16px]"
-          title="Add task"
+          className='mt-[2px] mr-3 text-[8px] sm:text-[10px] lg:text-[16px]'
+          title='+ Add Task'
         />
-        <SelectButton className="w-[90px] sm:w-[155px]" options={options} setFilter={setFilter} defaultValue={{ value: "all", label: "All" }} onChange={newValue => setFilter(newValue.value)} width='150px'/>
+        <SelectButton
+          className='w-[90px] sm:w-[155px]'
+          options={options}
+          setFilter={setFilter}
+          defaultValue={{ value: 'all', label: 'All' }}
+          onChange={(newValue) => setFilter(newValue.value)}
+          width='150px'
+        />
       </div>
     </div>
   );
