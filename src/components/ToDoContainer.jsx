7218e59b4b998a3eb7bbc693f1bde90d1a73f8f0
Aditya Shelke <adi.cs.night@gmail.com>
2022-10-12 22:36:46 +0530

    Filtered list are responsive

--- src/components/ToDoContainer.jsx
+++ src/components/ToDoContainer.jsx
@@ -64,26 +64,27 @@ export default function ToDoContainer() {
 
   const filteredList = todos?.map((todo, index) =>
     todo.status === filter ? (
-      <li
-        key={todo.id}
-        className='bg-white border-b-2 py-1 px-2 flex  rounded-lg mb-3 ml-auto mr-auto mt-4'
-        style={{ width: '40vw' }}
-      >
-        <input
-          type='checkbox'
-          className='h-7 w-7 mt-auto mb-auto mr-2 ml-2 rounded-full text-[#4d3434] focus:ring-[#757575]'
-          id={index}
-          checked={checkedOrNot(todo)}
-          onChange={checkboxhandler}
-        />
-        <div className='text-[#676767] font-bold mr-auto p-2'>{todo.title}</div>
-        <div className='p-2 mr-4 cursor-pointer'>
-          <FontAwesomeIcon className='w-5 h-5' icon={faTrashCan} />
-        </div>
-        <div className='p-2 cursor-pointer'>
-          <FontAwesomeIcon className='w-5 h-5' icon={faPenToSquare} />
-        </div>
-      </li>
+      <div key={index} className='px-2'>
+        <li
+          key={todo.id}
+          className='bg-white items-center border-b-2 py-0 md:py-2 px-2 md:px-5 flex rounded-lg mb-3 ml-auto mr-auto mt-4'
+        >
+          <input
+            type='checkbox'
+            className='h-4 w-4 md:h-7 md:w-7 mt-auto mb-auto mr-2 ml-2 rounded-full text-[#4d3434] focus:ring-[#757575]'
+            id={index}
+            checked={checkedOrNot(todo)}
+            onChange={checkboxhandler}
+          />
+          <div className='text-[#676767] font-light md:font-bold mr-auto p-2 md:text-sm text-[12px]'>
+            {todo.title}
+          </div>
+          <div className='text-[13.5px] md:text-base cursor-pointer md:space-x-12 space-x-4 justify-items-center flex '>
+            <FontAwesomeIcon onClick={() => handleDelete(index)} icon={faTrashCan} />
+            <FontAwesomeIcon onClick={() => handleEdit(index)} icon={faPenToSquare} />
+          </div>
+        </li>
+      </div>
     ) : (
       ''
 
