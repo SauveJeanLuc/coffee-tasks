edc9be244620edf33bfc7b6eecb9def7231ddff4
Aditya Shelke <adi.cs.night@gmail.com>
2022-10-13 01:35:51 +0530

    optimised and readable code, seperate component for todoItem

--- src/components/ToDoContainer.jsx
+++ src/components/ToDoContainer.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Nav from './Nav';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPenToSquare, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 import PopUp from './PopUp';
+import Todo from './Todo';
 
 export default function ToDoContainer() {
   const [popUpvisible, setPopUpvisible] = useState(false);
@@ -64,27 +63,15 @@ export default function ToDoContainer() {
 
   const filteredList = todos?.map((todo, index) =>
     todo.status === filter ? (
-      <div key={index} className='px-2'>
-        <li
-          key={todo.id}
-          className='bg-white items-center border-b-2 py-0 md:py-2 px-2 md:px-5 flex rounded-lg mb-3 ml-auto mr-auto mt-4'
-        >
-          <input
-            type='checkbox'
-            className='h-4 w-4 md:h-7 md:w-7 mt-auto mb-auto mr-2 ml-2 rounded-full text-[#4d3434] focus:ring-[#757575]'
-            id={index}
-            checked={checkedOrNot(todo)}
-            onChange={checkboxhandler}
-          />
-          <div className='text-[#676767] font-light md:font-bold mr-auto p-2 md:text-sm text-[12px]'>
-            {todo.title}
-          </div>
-          <div className='text-[13.5px] md:text-base cursor-pointer md:space-x-12 space-x-4 justify-items-center flex '>
-            <FontAwesomeIcon onClick={() => handleDelete(index)} icon={faTrashCan} />
-            <FontAwesomeIcon onClick={() => handleEdit(index)} icon={faPenToSquare} />
-          </div>
-        </li>
-      </div>
+      <Todo
+        key={index}
+        index={index}
+        todo={todo}
+        checkedOrNot={checkedOrNot}
+        checkboxhandler={checkboxhandler}
+        handleEdit={handleEdit}
+        handleDelete={handleDelete}
+      />
     ) : (
       ''
 
@@ -93,27 +80,15 @@ export default function ToDoContainer() {
   );
 
   const allList = todos?.map((todo, index) => (
-    <div key={index} className='px-2'>
-      <li
-        key={todo.id}
-        className='bg-white items-center border-b-2 py-0 md:py-2 px-2 md:px-5 flex rounded-lg mb-3 ml-auto mr-auto mt-4'
-      >
-        <input
-          type='checkbox'
-          className='h-4 w-4 md:h-7 md:w-7 mt-auto mb-auto mr-2 ml-2 rounded-full text-[#4d3434] focus:ring-[#757575]'
-          id={index}
-          checked={checkedOrNot(todo)}
-          onChange={checkboxhandler}
-        />
-        <div className='text-[#676767] font-light md:font-bold mr-auto p-2 md:text-sm text-[12px]'>
-          {todo.title}
-        </div>
-        <div className='text-[13.5px] md:text-base cursor-pointer md:space-x-12 space-x-4 justify-items-center flex '>
-          <FontAwesomeIcon onClick={() => handleDelete(index)} icon={faTrashCan} />
-          <FontAwesomeIcon onClick={() => handleEdit(index)} icon={faPenToSquare} />
-        </div>
-      </li>
-    </div>
+    <Todo
+      key={index}
+      index={index}
+      todo={todo}
+      checkedOrNot={checkedOrNot}
+      checkboxhandler={checkboxhandler}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
   ));
   return (
     <>
